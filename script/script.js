8c745f6893a3b026bd54eca153e5f9cbf420e9dd
shuvo01832 <mdshvaslam@gmail.com>
2026-03-06 01:05:32 +0600

    English janala

--- script/script.js
+++ script/script.js
@@ -1,7 +1,49 @@
 const loadLessons = () => {
-    fetch ("https://openapi.programming-hero.com/api/levels/all")
-    .then((res) => res.json())
-    .then((json) =>  displayLesson(json.data));
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+        .then((res) => res.json())
+        .then((json) => displayLesson(json.data));
+};
+
+const loadlevelword = (id) => {
+    console.log(id);
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => displayLevelword(data.data));
+};
+const displayLevelword = (words) => {
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = " ";
+
+    if (words.length == 0) {
+        wordContainer.innerHTML = `
+       <div class="text-center  col-span-full space-y-4">
+       <img class="mx-auto" src="./assets/alert-error.png"/>
+      <p class="font-semibold text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+      <h2 class="text-4xl">নেক্সট Lesson এ যান</h2>
+     </div>
+
+       `;
+        return;
+    }
+
+    words.forEach((word) => {
+        console.log(word);
+        const card = document.createElement("div");
+        card.innerHTML = `
+     <div class="bg-white rounded-lg shadow-sm py-10 px-5 text-center">
+        <h2 class="font-bold text-2xl ">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
+        <p class="font-semibold">Meaning /Pronounciation</p>
+        <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায়নি"}</div>
+        <div class="flex justify-between items-center">
+        <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]" ><i class="fa-solid fa-circle-info"></i></button>
+        <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]" ><i class="fa-solid fa-volume-high"></i></button>
+        </div>
+      </div>
+
+    `;
+        wordContainer.append(card);
+    });
 };
 
 const displayLesson = (lessons) => {
@@ -10,12 +52,12 @@ const displayLesson = (lessons) => {
     levelContainer.innerHTML = "";
     // 2.get into every lessons
 
-    for (let lesson of lessons){
-            // 3.create Element
-            // console.log(lesson);
-            const btnDiv = document.createElement("div");
-            btnDiv.innerHTML=`
-            <button class="btn btn-outline btn-primary">
+    for (let lesson of lessons) {
+        // 3.create Element
+        // console.log(lesson);
+        const btnDiv = document.createElement("div");
+        btnDiv.innerHTML = `
+            <button onclick="loadlevelword(${lesson.level_no})" class="btn btn-outline btn-primary">
             <i class="fa-solid fa-book-open"></i>Lesson -${lesson.level_no}
             </button>
             `;
